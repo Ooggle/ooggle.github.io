806f6026c59ba83fb9e97327034b1aa8a9d9c706
Ooggle <anto.saglier@gmail.com>
2020-06-19 19:00:57 +0200

    first pre-release

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,18 @@
 document.addEventListener("scroll", scrollcheck);
 
+/* shadow navbar on scroll */
 function scrollcheck() {
     if(window.pageYOffset > 20) {
+        /* Shadow over the navbar */
         document.getElementById("site-navbar").style.boxShadow = "0 8px 17px 2px rgba(0,0,0,0.14), 0 3px 14px 2px rgba(0,0,0,0.12), 0 5px 5px -3px rgba(0,0,0,0.2)";
+
+
+        document.getElementById("site-navbar-title").style.borderBottomLeftRadius = "8px";
+        document.getElementById("site-navbar-last-cat").style.borderBottomRightRadius = "8px";
     } else {
         document.getElementById("site-navbar").style.boxShadow = "";
+        
+        document.getElementById("site-navbar-title").style.borderBottomLeftRadius = "0";
+        document.getElementById("site-navbar-last-cat").style.borderBottomRightRadius = "0";
     }
 }
